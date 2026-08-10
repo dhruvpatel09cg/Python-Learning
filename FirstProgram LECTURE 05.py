3f395f6bfaf8ed54d2f1313b6ce49c4b9f1e8eef
dhruvpatel09cg <dhruv.patel.cg@gmail.com>
2026-08-10 20:09:05 +0530

    new

--- FirstProgram LECTURE 05.py
+++ FirstProgram LECTURE 05.py
@@ -214,6 +214,41 @@ if i > 5:
 print("don't wanna fill it")
 
 # WAP 1
-while n < 10:
-    print(n + n)
-    n += 1
+n = 10
+
+#for loop
+sum = 0
+for i in range(1, n+1):
+    sum += i
+
+print("total sum" , sum)
+
+#while loop
+n = 15
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+print("total sum =", sum) 
+
+#wap 2
+
+#factorial
+
+#while
+n = 6
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+print("fact =", fact)
+
+#for
+n = 8
+fact = 1
+
+for i in range(1, n+1):
+    fact *= i
+print("fact =", fact)
