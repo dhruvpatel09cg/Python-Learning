e6900c1d82ef1f81457cff298cca27580ea18988
dhruvpatel09cg <dhruv.patel.cg@gmail.com>
2026-09-23 20:47:45 +0530

    Polymorphism

--- FirstProgram LECTURE 09.py
+++ FirstProgram LECTURE 09.py
@@ -173,3 +173,52 @@ print(s1.percentage) # After using this the marks are changing but percentage re
 # s1.calcper()
 # print(s1.percentage) # After using property no need to use this
 
+'''@getter & @setter to study'''
+
+# Polymorphism
+# --> Operator Overloading
+# function of different operator different according to different data type:
+print(1 + 2) #Add
+print("Dhruv " + "Patel") #concatenate
+print([1, 2, 3] + [4, 5, 6]) #merge
+# + did different function acc. to different data types
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real, "i +", self.img, "j")
+
+    def add(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img+ num2.img
+        return Complex(newReal, newImg) # Alternate of Dunder
+
+    def __add__(self, num2):
+            newReal = self.real + num2.real
+            newImg = self.img+ num2.img
+            return Complex(newReal, newImg)
+
+    def __sub__(self, num2):
+                newReal = self.real - num2.real
+                newImg = self.img - num2.img
+                return Complex(newReal, newImg)
+
+num1 = Complex(1, 4)
+num1.showNumber()
+
+num2 = Complex(6, 6)
+num2.showNumber()
+
+num3 = num1 + num2
+num3.showNumber()
+
+num3 = num1 - num2
+num3.showNumber()
+
+# num3 = num1.add(num2)
+# num3.showNumber() #no need to use this after Dunder
+# Dunder functions: used __ before
+
