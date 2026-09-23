80916bd243660a5520899ecc4da154f6b7124bdb
dhruvpatel09cg <dhruv.patel.cg@gmail.com>
2026-09-23 19:33:44 +0530

    super method

--- FirstProgram LECTURE 09.py
+++ FirstProgram LECTURE 09.py
@@ -99,3 +99,25 @@ print(c1.varA)
 
 b1 = B()
 print(b1.varB)
+
+# Super Method
+class Car:
+    def __init__(self, type):
+        self.type = type
+
+    @staticmethod
+    def start():
+        print("car started")
+
+    @staticmethod
+    def stop():
+        print("car stopped.")
+
+class Toyota(Car):
+    def __init__(self, name, type):
+        self.name = name
+        super().__init__(type)
+        super().start()
+
+car1 = Toyota("Hilux", "Hybrid")
+print(car1.name, car1.type)
