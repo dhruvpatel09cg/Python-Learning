ebc91296fb277ec4d10e8b33f28aecff774ce823
dhruvpatel09cg <dhruv.patel.cg@gmail.com>
2026-09-23 19:24:26 +0530

    multiple inheritance

--- FirstProgram LECTURE 09.py
+++ FirstProgram LECTURE 09.py
@@ -81,3 +81,21 @@ class LC300(Toyota):
 
 car1 = LC300("Petrol")
 print(car1.f_type, car1.brand)
+
+# --> Multiple Inheritance
+class A:
+    varA = "welcome to class A"
+
+class B:
+    varB = "welcome to class B"
+
+class C(A, B):
+    varC = "welcome to class C"
+
+c1 = C()
+print(c1.varC)
+print(c1.varB)
+print(c1.varA)
+
+b1 = B()
+print(b1.varB)
